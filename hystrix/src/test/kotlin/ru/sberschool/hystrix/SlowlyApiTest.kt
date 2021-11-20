e7d5ecc41e06a3063e3d584d17029a82b54bc2a9
Kirill Vasilev <kirill.vasilev.work@gmail.com>
2021-11-20 22:32:16 +0300

    hystrix done

--- hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
+++ hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
@@ -7,25 +7,31 @@ import feign.jackson.JacksonDecoder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockserver.client.server.MockServerClient
+import org.mockserver.client.MockServerClient
 import org.mockserver.integration.ClientAndServer
 import org.mockserver.model.HttpRequest
 import org.mockserver.model.HttpResponse
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class SlowlyApiTest {
-    val client = HystrixFeign.builder()
+    val clientMock = HystrixFeign.builder()
         .client(ApacheHttpClient())
         .decoder(JacksonDecoder())
-        // для удобства тестирования задаем таймауты на 1 секунду
         .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
         .target(SlowlyApi::class.java, "http://127.0.0.1:18080", FallbackSlowlyApi())
+
+    val clientPokeapi = HystrixFeign.builder()
+        .client(ApacheHttpClient())
+        .decoder(JacksonDecoder())
+        .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
+        .target(SlowlyApi::class.java, "https://pokeapi.co/api/v2/", FallbackSlowlyApi())
+
     lateinit var mockServer: ClientAndServer
 
     @BeforeEach
     fun setup() {
-        // запускаем мок сервер для тестирования клиента
         mockServer = ClientAndServer.startClientAndServer(18080)
     }
 
@@ -35,22 +41,55 @@ class SlowlyApiTest {
     }
 
     @Test
-    fun `getSomething() should return predefined data`() {
-        // given
-        MockServerClient("127.0.0.1", 18080)
+    fun `should return fallback`() {
+        MockServerClient("127.0.01", 18080)
             .`when`(
-                // задаем матчер для нашего запроса
                 HttpRequest.request()
                     .withMethod("GET")
                     .withPath("/")
             )
             .respond(
-                // наш запрос попадает на таймаут
                 HttpResponse.response()
                     .withStatusCode(400)
-                    .withDelay(TimeUnit.SECONDS, 30) //
+                    .withDelay(TimeUnit.SECONDS, 30)
+                    .withBody(
+                        "{\n" +
+                                "  \"name\": \"ditto\",\n" +
+                                "  \"count\": 7\n" +
+                                "}"
+                    )
             )
-        // expect
-        assertEquals("predefined data", client.getSomething().data)
+        assertEquals("fallback", clientMock.getPokemon().pokemonName)
+    }
+
+    @Test
+    fun `should return right pokemon name using mock`() {
+        MockServerClient("127.0.0.1", 18080)
+            .`when`(
+                HttpRequest.request()
+                    .withMethod("GET")
+                    .withPath("/pokemon/ditto")
+            )
+            .respond(
+                HttpResponse.response()
+                    .withStatusCode(200)
+                    .withBody(
+                        "{\n" +
+                                "  \"name\": \"ditto\",\n" +
+                                "  \"count\": 7\n" +
+                                "}"
+                    )
+            )
+        assertEquals("ditto", clientMock.getPokemon().pokemonName)
+    }
+
+    @Test
+    fun `should return wrong pokemon name using api`() {
+        assertNotEquals("pikachu", clientPokeapi.getPokemon().pokemonName)
+    }
+
+    @Test
+    fun `should return right pokemon name using api`() {
+        assertEquals("ditto", clientPokeapi.getPokemon().pokemonName)
     }
 }
